3856da364242138f5f4820cbe01c63f1b347b37b
rusyk96 <rusyabar.rb@gmail.com>
2026-07-26 22:16:07 +0300

    Global_upgrate

--- Frontend/Component_Based_Design/focus_component/galerey/bento_grid/patterns/fantome_helpers.js
+++
@@ -1,2 +0,0 @@
-import { createCardHtml } from '../../../../../apps/modules/bento_grid/grid/bento-helpers.js';
-export { createCardHtml };
--- Frontend/Component_Based_Design/focus_component/galerey/bento_grid/patterns/index_patterns.js
+++
@@ -1,6 +0,0 @@
-export { renderPattern1 } from './pattern1/pattern1.js';
-export { renderPattern2 } from './pattern2/pattern2.js';
-export { renderPattern3 } from './pattern3/pattern3.js';
-// Когда будем добавлять новые — просто дописываем строчки:
-// export { renderPattern2 } from './pattern2/pattern2.js';
-// export { renderPattern3 } from './pattern3/pattern3.js';
